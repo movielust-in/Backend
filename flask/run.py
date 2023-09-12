5857485bdeac135000043f7a58b6a0e080163212
Anurag Parmar <01anuragparmar@gmail.com>
2023-09-12 19:05:05 +0530

    fixed: mangets links

--- flask/run.py
+++ flask/run.py
@@ -4,4 +4,4 @@ from src import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5000)
--- node/src/controllers/torrents.controller.js
+++ node/src/controllers/torrents.controller.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { Movie } from "../models/torrent.model.js";
+import { magnetFromHash } from "../magnetFromHash.js";
 
 export const YTSMovieMagnet = async (req, res) => {
   try {
     // console.log("Getting movie torrents");
     const imdb_id = req.params.imdb_id;
     const tmdb_id = req.params.tmdb_id;
-    let arr = [];
-    let movie_json;
 
     const check_exist = await Movie.findOne({ id: tmdb_id });
 
@@ -15,35 +14,32 @@ export const YTSMovieMagnet = async (req, res) => {
       return res.send(check_exist.links);
     }
 
-    await axios
-      .get(`https://yts.mx/api/v2/movie_details.json?imdb_id=${imdb_id}`)
-      .then((res) => {
-        movie_json = res.data.data.movie;
-        const movie_torrent = movie_json.torrents;
-        movie_torrent.map((torrents) => {
-          let dict = {
-            quality: torrents.quality,
-            magnet: torrents.url,
-            size: torrents.size,
-            size_bytes: torrents.size_bytes,
-            hash: torrents.hash,
-          };
-          arr.push(dict);
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const ytsRes = await axios.get(
+      `https://yts.mx/api/v2/movie_details.json?imdb_id=${imdb_id}`
+    );
 
-    if (arr.length > 0) {
-      await Movie.updateOne(
-        { id: tmdb_id, name: movie_json.title },
-        { $set: { links: arr } }
-      );
-      return res.send(arr);
-    } else {
-      return res.send(false);
+    const movieData = ytsRes.data.data.movie;
+    const movieTorrents = movieData.torrents;
+    const mangets = movieTorrents.map((torrent) => {
+      return {
+        quality: torrent.quality,
+        magnet: magnetFromHash(torrent.hash, movieData.title, torrent.quality),
+        size: torrent.size,
+        size_bytes: torrent.size_bytes,
+        hash: torrent.hash,
+      };
+    });
+
+    if (mangets.length <= 0) {
+      return res.status(404).send(false);
     }
+
+    await Movie.updateOne(
+      { id: tmdb_id, name: movieData.title },
+      { $set: { links: mangets } }
+    );
+
+    return res.send(mangets);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
