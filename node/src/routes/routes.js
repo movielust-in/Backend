5243e04222744838e264439da6069778f546b5d9
Anurag Parmar <01anuragparmar@gmail.com>
2023-09-12 12:01:06 +0530

    fix: removed imports to removed functions.

--- node/src/routes/routes.js
+++ node/src/routes/routes.js
@@ -4,11 +4,7 @@ import authRouter from "./auth.routes.js";
 import adminRouter from "./admin.routes.js";
 import torrentRouter from "./torrent.routes.js";
 
-import {
-  getAllAvatar,
-  imdbRating,
-  imdbRatings,
-} from "../controllers/main.controller.js";
+import { getAllAvatar } from "../controllers/main.controller.js";
 
 const mainRouter = Router();
 
@@ -18,7 +14,5 @@ mainRouter.use("/auth", authRouter);
 mainRouter.use("/torrent", torrentRouter);
 
 mainRouter.get("/avatar/getall", getAllAvatar);
-mainRouter.get("/movie/imdb-rating/:id", imdbRating);
-mainRouter.get("/movie/imdb-ratings/:id", imdbRatings);
 
 export default mainRouter;
