93db678f9e2f79f033fb557fc5b3479014de019c
Anurag Parmar <01anuragparmar@gmail.com>
2023-10-16 22:44:18 +0530

    node: create server.js

--- node/src/app.js
+++ node/src/app.js
@@ -1,17 +1,10 @@
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-import dotenv from "dotenv";
 import express, { json } from "express";
 
-import { verifyTransport } from "./helpers/nodemailer.js";
 import mainRouter from "./routes/routes.js";
 import flaskProxy from "./middlewares/flask-proxy.middleware.js";
-import { connectDB } from "./helpers/db.js";
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -29,15 +22,4 @@ app.use("/", (req, res) => {
   res.status(200).send({ status: "Server is running" });
 });
 
-await connectDB();
-
-await verifyTransport();
-
-
-app
-  .listen(PORT, () => {
-    console.log("Server is running on port:", PORT);
-  })
-  .on("err", (err) => {
-    console.log("Server error.", err);
-  });
+export default app;
---
+++ node/src/server.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+
+import app from "./app.js";
+import { connectDB } from "./helpers/db.js";
+import { verifyTransport } from "./helpers/nodemailer.js";
+
+dotenv.config();
+
+await connectDB();
+
+await verifyTransport();
+
+const PORT = process.env.PORT || 3001;
+
+const server = app.listen(PORT, () => {
+    console.log("Listening at", PORT);
+});
+
+const exitHandler = () => {
+    if (!server) return process.exit(1);
+
+    server.close(() => {
+        console.log("Server closed.");
+        process.exit(1);
+    });
+};
+
+const unexpectedErrorHandler = (error) => {
+    console.error(error);
+    exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM RECIEVED.");
+    if (server) server.close();
+});
